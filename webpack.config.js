87f1bc03e0690bbabd4b90dab4b116b6aee56e9c
Paolo Bueno <paolohaji@gmail.com>
2018-08-27 16:20:34 +0200

    Add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
@@ -42,15 +43,22 @@ module.exports = {
         exclude: /node_modules/,
         test: /\.js$/, loader: "source-map-loader"
       },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
     ]
   },
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+    }),
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
 };
